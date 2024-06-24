b397c46cab65bc7ceeefe7c9287842af7ec604f7
Anya Zeng <anya.tseng@gmail.com>
2024-06-24 16:41:47 +1000

    feat: build the structur

---
+++ src/Components/NavBar/Components/NumResults.js
@@ -0,0 +1,11 @@
+import React, { Children } from "react";
+
+function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
+
+export default NumResults;
