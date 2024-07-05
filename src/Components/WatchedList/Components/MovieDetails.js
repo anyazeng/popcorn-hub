0019f82d9d520f83c97b30ba4108dd1052140053
Anya Zeng <anya.tseng@gmail.com>
2024-07-05 15:39:18 +1000

    feat: display movie details by selecting movies

--- src/Components/WatchedList/Components/MovieDetails.js
+++ src/Components/WatchedList/Components/MovieDetails.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const KEY = "6a15263b";
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      setMovie(data);
+    }
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      <div>{selectedId}</div>
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`Poster of ${title}`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐️</span>
+            <span>{`${imdbRating} IMDb rating`}</span>
+          </p>
+        </div>
+      </header>
+      <section>
+        <p>
+          <i>{plot}</i>
+        </p>
+        <p>{actors}</p>
+        <p>{director}</p>
+      </section>
     </div>
   );
 }
